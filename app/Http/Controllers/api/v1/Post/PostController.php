6429399bf3b5895b1add09dc48dffbc55f3fe3fb
Design 1 <design1@iroute.in>
2019-12-20 12:26:27 +0530

    add pid in activity

--- app/Http/Controllers/api/v1/Post/PostController.php
+++ app/Http/Controllers/api/v1/Post/PostController.php
@@ -96,7 +96,7 @@ public $successStatus = 200;
             $arr['vendorDetail'] = Vendor::find($postValue->store_id);
             $arr['mainCategory'] = MainCategory::find($postValue->product_interest_new);
             $arr['subCategory'] = SubCategory::find($postValue->user_interest);
-            $arr['Activities'] = CategoryActivity::where('interest_id','=',$postValue->product_interest_new)->where('category_id','=',$postValue->user_interest)->orWhere('category_id','=',0)->where('status','=',1)->get(['reason_id','reason_name','icon','interest_id',DB:raw($postValue->pid.' as post_id')]);
+            $arr['Activities'] = CategoryActivity::where('interest_id','=',$postValue->product_interest_new)->where('category_id','=',$postValue->user_interest)->orWhere('category_id','=',0)->where('status','=',1)->get(['reason_id','reason_name','icon','interest_id',DB::raw($postValue->pid.' as post_id')]);
             
             //$arr['postComments'] = PostComments::where('cpid','=',$postValue->pid)->get(['cid','comment','user_id']);
             $postArray[]=$arr;
